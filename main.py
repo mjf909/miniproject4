cf4b887d40e53b27e67565ff0bf247d697f9d8ff
Michael Flavin <mjflavin@mail.fhsu.edu>
2022-12-15 16:01:45 -0600

    added first path

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.contrib import admin
+from django.urls import path, include
 
-
-
-
-
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
+]
 
 class PagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
